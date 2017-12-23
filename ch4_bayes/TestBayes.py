e777124ab384ffae8fd3b8da470ec2c3f6752f0f
zhongweiwu <zhongweiwu@pptv.com>
2017-12-23 21:45:19 +0800

    modify README.MD

--- ch4_bayes/TestBayes.py
+++ ch4_bayes/TestBayes.py
@@ -1,5 +1,17 @@
 from ch4_bayes import bayes
 
-listOPosts, listClasses = bayes.loadDataSet()
-myVoacbList = bayes.createVocabList(listOPosts)
-print(myVoacbList)
+testDoc1 = ['love', 'my', 'dalmation']
+testDoc2 = ['dog', 'my', 'love']
+classLable = bayes.testingNB(testDoc1)
+print(classLable)
+# listOPosts, listClasses = bayes.loadDataSet()
+# myVoacbList = bayes.createVocabList(listOPosts)
+# returnVec = bayes.setOfWords2Vec(myVoacbList, listOPosts[0])
+# trainMat = []
+# for postinDoc in listOPosts:
+#     trainMat.append(bayes.setOfWords2Vec(myVoacbList, postinDoc))
+#
+# p0V, p1V, pAb = bayes.trainNB0(trainMat, listClasses)
+# print('%s\n%s\n%s' % (p0V, p1V, pAb))
+# print(returnVec)
+# print(sorted(myVoacbList))
--- ch4_bayes/bayes.py
+++ ch4_bayes/bayes.py
@@ -1,3 +1,6 @@
+from numpy import *
+
+
 def loadDataSet():
     """
     创建文档实验样本
@@ -24,18 +27,20 @@ def createVocabList(dataSet):
 
     vocabSet = set()
     for doc in dataSet:
-        vocabSet |= set(doc)#|为集合并
+        vocabSet |= set(doc)  # |为集合并
     return list(vocabSet)
 
 
 def setOfWords2Vec(vocabList, inputSet):
     """
+    词集模型：一个词在文档中只出现一次
     输出文档向量对应着词汇表中的词是否在文档中出现
     :param vocabList:
     :param inputSet:
     :return:
     """
-    returnVec = [0]*len(vocabList)
+
+    returnVec = [0] * len(vocabList)
     for word in inputSet:
         if word in vocabList:
             returnVec[vocabList.index(word)] = 1
@@ -44,5 +49,90 @@ def setOfWords2Vec(vocabList, inputSet):
     return returnVec
 
 
+def bagOfWords2Vec(vocabList, inputSet):
+    """
+    词袋模型：一个词在文档可出现多次
+    输出文档向量对应着词汇表中的词是否在文档中出现
+    :param vocabList:
+    :param inputSet:
+    :return:
+    """
+
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
 
 
+def trainNB0(trainMatrix, trainCategory):
+    """
+    朴素贝叶斯分类器训练函数
+    :param trainMatrix:
+    :param trainCategory:
+    :return:
+    """
+
+    # trainMatrix的维数为所有文档中不重复词的数量
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    # 此处不修正，但应该修正
+    pAbusvie = sum(trainCategory) / float(numTrainDocs)
+    # piNum为所有i类文档中所有词出现的数量和向量
+    # piDenom为所有i类文档中所有词的数量和（词只出现一次）
+    # 即sum(piNum) = piDenom
+
+    # 若出现概率为0的情况则应使用拉普拉斯修正,此例中不会出现，若词典词数大于训练词
+    # 数则可能会出现，而此例中词典词数和训练词数相同
+    #p0Num, p1Num = zeros(numWords), zeros(numWords)
+    p0Num, p1Num = ones(numWords), ones(numWords)
+    p0Denom, p1Denom = 0.0, 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    # 若出现下溢出则使用对数化
+    p1Vect = log(p1Num/p1Denom)
+    p0Vect = log(p0Num/p0Denom)
+    #p1Vect = p1Num / p1Denom
+    #p0Vect = p0Num / p0Denom
+    return p0Vect, p1Vect, pAbusvie
+
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    """
+    朴素贝叶斯分类函数
+    :param vec2Classify:
+    :param p0Vec:
+    :param p1Vec:
+    :param pClass1:
+    :return:
+    """
+
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB(testDoc):
+    """
+    朴素贝叶斯训练函数
+    :param testDoc:
+    :return:
+    """
+
+    listOPosts, listClasses = loadDataSet()
+    myVoacbList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVoacbList, postinDoc))
+    p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+    # 必须使用array，否则不能使用内积
+    docVec = array(setOfWords2Vec(myVoacbList, testDoc))
+    return classifyNB(docVec, p0V, p1V, pAb)
