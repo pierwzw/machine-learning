0005b935d7c0fc14fb8b21ec9dc9ecd4adf02cca
zhongweiwu <zhongweiwu@pptv.com>
2018-01-26 10:19:01 +0800

    svm

--- ch3_decisiontree/TestLenses.py
+++ ch3_decisiontree/TestLenses.py
@@ -1,8 +1,14 @@
 from ch3_decisiontree import trees, treePlotter
 
-fr = open('contact_lenses\\lenses.txt')
-lenses = [inst.strip().split('\t') for inst in fr.readlines()]
-lensesLables = ['age', 'prescript', 'astigmatic', 'tearRate']
-lensesTree = trees.createTree(lenses, lensesLables)
-#print(lensesTree)
-treePlotter.createPlot(lensesTree)
+# fr = open('contact_lenses\\lenses.txt')
+# lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+# lensesLables = ['age', 'prescript', 'astigmatic', 'tearRate']
+# lensesTree = trees.createTree(lenses, lensesLables)
+# #print(lensesTree)
+# treePlotter.createPlot(lensesTree)
+
+dataset = [['di','bei', 'bo','di2'],
+           ['gao','bei','ben','zhong2'],
+           ['di', 'nan', 'ben','gao2'],
+           ['gao','bei','yan', 'zhong2']]
+print(trees.chooseBestFeatureToSplit(dataset))
